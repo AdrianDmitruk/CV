cca5991a7fdc816e2fb96e608485befd86aa85c6
Adrian <110024387+AdrianDmitruk@users.noreply.github.com>
2022-11-06 02:23:23 +0300

    Delete static directory

--- static/js/370.877ca05c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcv=self.webpackChunkcv||[]).push([[370],{370:function(e,c,s){s.r(c),s.d(c,{default:function(){return a}});s(791);var n=s(125),t=s(184),a=function(){return(0,t.jsx)("section",{className:"contects-page",children:(0,t.jsxs)("div",{className:"container",children:[(0,t.jsxs)("h3",{className:"contects-page__title page-title",children:[(0,t.jsx)("span",{children:"/"}),"contacts"]}),(0,t.jsx)(n.Ow,{})]})})}}}]);
-//# sourceMappingURL=370.877ca05c.chunk.js.map
