9e8c407972c397d48bb3800bd8b8fdbf01c415e3
Adrian <dmitrukadrian@gmail.com>
2022-11-06 20:07:49 +0300

    fix en

--- src/18n.js
+++ src/18n.js
@@ -3,6 +3,17 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 
+import translationEN from "../src/locales/en/translation.json";
+import translationRU from "../src/locales/ru/translation.json";
+
+const resources = {
+    en: {
+        translation: translationEN
+    },
+    ru: {
+        translation: translationRU
+    }
+};
 
 i18n
     .use(Backend).use(LanguageDetector).use(initReactI18next).init({
@@ -14,6 +25,8 @@ i18n
         },
         interpolation: {
             escapeValue: false,
-        }
+        },
+        resources,
+        lng: "en",
     });
 export default i18n;
