255664dac7a533a8c277ba58c650592843ede1f2
Adrian <dmitrukadrian@gmail.com>
2022-11-07 01:45:24 +0300

    spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import '../scss/section/spinner.scss'
+
 const Spinner = () => {
-  return <div>Загрузка</div>
+  return (
+    <div className="spinner">
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default Spinner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <First />
-      {/* <Quote /> */}
+      {/* <Quote />  */}
       <Projects />
       <Skills />
       <AboutMain />
