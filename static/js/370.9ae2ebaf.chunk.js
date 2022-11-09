c2fac9a271356b3342bd2cbd7bd6701eb10396fc
Adrian <dmitrukadrian@gmail.com>
2022-11-10 00:06:35 +0300

    Updates

---
+++ static/js/370.9ae2ebaf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcv=self.webpackChunkcv||[]).push([[370],{370:function(e,c,s){s.r(c),s.d(c,{default:function(){return a}});s(791);var n=s(173),t=s(184),a=function(){return(0,t.jsx)("section",{className:"contects-page",children:(0,t.jsxs)("div",{className:"container",children:[(0,t.jsxs)("h3",{className:"contects-page__title page-title",children:[(0,t.jsx)("span",{children:"/"}),"contacts"]}),(0,t.jsx)(n.Ow,{})]})})}}}]);
+//# sourceMappingURL=370.9ae2ebaf.chunk.js.map
